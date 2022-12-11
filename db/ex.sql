24305204c8dd7feb76e50135c9ba6437b483b5ff
cherylregar <cheryllidia99@gmail.com>
2022-12-11 16:12:48 +0700

    INI YAH

--- db/ex.sql
+++ db/ex.sql
@@ -22,3 +22,51 @@ statusEvent varchar(25),
 constraint event_PK primary key (kdEvent),
 constraint user_event_FK foreign key (username) references user(username)
 );
+
+/* Table Ticket Chategory */
+
+create table ticketChategory
+(kdJenis char(5) not null,
+jenisTiket varchar (10) not null,
+kdEvent char(5) not null,
+harga int (8),
+kapasitasTiket int(3),
+constraint ticketChategory_PK primary key (kdJenis), 
+constraint  event_FK foreign key (kdEvent) references event(kdEvent)
+);
+
+/* Table Ticket */
+
+create table ticket
+(kdTiket char(13) not null,
+kdJenis char(5) not null,
+constraint ticket_PK primary key (kdTiket),
+constraint ticketChategory_FK foreign key (kdJenis) references ticketChategory (kdJenis)
+);
+
+
+/* Table Order Detail */
+
+create table orderDetail
+(kdTiket char(13) not null,
+kdPemesanan char(10) not null,
+constraint orderDetail_PK primary key (kdTiket, kdPemesanan),
+constraint ticket_FK foreign key (kdTiket) references ticket (kdTiket),
+constraint order_FK foreign key (kdPemesanan) references order (kdPemesanan)
+
+);
+
+/* Table Order */
+
+create table order
+(kdPemesanan char(10) not null,
+username varchar(25) not null,
+fullNameOrder varchar(30),
+statusBayar varchar(15),
+totalBayar int(9), 
+metodePembayaran varchar(15),
+buktiBayar text,
+noRek int(18) not null,
+constraint order_PK primary key (kdPemesanan),
+constraint user_FK foreign key (username) references user (username)
+);
